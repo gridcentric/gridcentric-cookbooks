0d39cbc2e1f46a9417fc0f0724b0f07dd26e44a5
Rahat Mahmood <rmahmood@gridcentric.ca>
2013-04-09 16:49:51 -0400

    Importing canary cookbook and updating roles.

--- roles/gridcentric-api.rb
+++ roles/gridcentric-api.rb
@@ -2,5 +2,7 @@ name "gridcentric-api"
 description "Role for a vms api client."
 
 run_list(
-    "recipe[vms::api]"
+    "recipe[vms::api]",
+    "recipe[canary::api]",
+    "recipe[canary::horizon]"
 )
